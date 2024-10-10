8ea5f2da7e18a8893c2cdbb03ee54577fe48f217
Abhay Sharma <abhay11042004@gmail.com>
2024-10-10 21:25:34 +0530

    Add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-
-
 import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-
 import Skills from "./components/Skills";
-
-import Aos from "aos";
-import "aos/dist/aos.css"
-
-import Contact from "./components/Contact";
 import Projects from "./components/Project";
-
+import Contact from "./components/Contact";
 
 const App = () => {
- useEffect(() => {
-  Aos.init();
-  
- }, [])
- 
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className="container">
         <Hero />
-        
         <Skills />
         <Projects />
         <Contact />
-       
       </div>
     </>
   );
